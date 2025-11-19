8a912a5bdae577d61145c0d273276edc9a0fe87f
Shira Kunik <shirakunik@MacBook-Air-sl-Shira.local>
2025-11-19 21:15:33 +0200

    completed assignment3

--- LoanCalc.java
+++ LoanCalc.java
@@ -29,7 +29,7 @@ public class LoanCalc {
 	// interest rate (as a percentage), the number of periods (n), and the periodical payment.
 	private static double endBalance(double loan, double rate, int n, double payment) {	
     for (int i = 0; i < n ; i++) {
-		loan = (loan - payment * (rate / 100 + 1) ); 
+		loan = (loan - payment) * (1+ rate / 100 ); 
 	}
 		return loan;
 	}
